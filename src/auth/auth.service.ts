171e94e5418e25f271914194686675d9baeed587
Ganesh-redaptive <99821803+Ganesh-redaptive@users.noreply.github.com>
2024-02-04 21:59:11 +0530

    error handelling in backend created.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Users } from './schema/auth.schema';
@@ -10,9 +10,11 @@ export class AuthService {
 
   async signIn(signInDto: SignInDto): Promise<Users> {
     const user = await this.model.findOne({ email: signInDto.email }).exec();
-    if (user?.password) {
-      throw new UnauthorizedException();
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
     return user;
   }
   async createUser(signUpDto: SignUpDto): Promise<Users> {
